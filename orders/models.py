65ede8054b9a66e7a281e857425e687ede9e8ea7
pilipchuk-philip <pilipchuk.philip@gmail.com>
2022-05-01 21:23:56 +0300

    order models added

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class OrderDetails(models.Model):
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+    product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    # def post_save(self, *args, **kwargs):
+        # !TODO for order details quantity delete product quantity
+        # breakpoint()
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
