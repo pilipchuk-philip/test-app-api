2d552baea7232a7cd50aa2129622400d92370f91
pilipchuk-philip <pilipchuk.philip@gmail.com>
2022-05-01 20:59:58 +0300

    Products model added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=120, verbose_name="Product Name")
+    price = models.DecimalField(decimal_places=2,
+            max_digits=10,
+            verbose_name="Product price")
+    quantity_in_stock = models.PositiveIntegerField(verbose_name="Product quantity")
+
+    def __str__(self):
+        return self.name
+
